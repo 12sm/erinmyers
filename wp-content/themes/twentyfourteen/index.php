2a9c4bcbf0d0191ee02147ea0160c7c460804406
Michael Shoup <michael@12southmusic.com>
2014-10-01 19:14:32 -0400

    Rock this up

---
+++ wp-content/themes/twentyfourteen/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<link rel='stylesheet' href='style.css'>
+<link rel="stylesheet" href="bootstrap-custom.css">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<div class="col-sm-6 col-sm-offset-3">
+		<img src="logo.png" class="img-responsive">
+			<h3 class="headline">Website Coming Soon...</h3>
+			<ul class="socials">
+				<li class="icons"><a href="https://www.facebook.com/pages/Erin-Myers-Spiral-Spine/" target="_blank"><i class='fa fa-facebook-square'></i></a></li>
+				<li class="icons"><a href="https://www.linkedin.com/profile/view?id=356451800" target="_blank"><i class="fa fa-linkedin-square"></i></a></li>
+				<li class="icons amazon"><a href="http://amzn.com/1475219466" target="_blank"><img src="amazon.png" class="img-responsive"></li>
+			</ul>
+</body>
