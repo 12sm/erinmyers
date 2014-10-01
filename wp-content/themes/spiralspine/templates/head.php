eb0221b4ff62e8a3e982637c8fee40a46171a1f0
michael <michael@12southmusic.com>
2014-10-01 18:50:39 -0500

    Google verification

--- wp-content/themes/spiralspine/templates/head.php
+++ wp-content/themes/spiralspine/templates/head.php
@@ -19,6 +19,7 @@ http://12southmusic.com/
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="google-site-verification" content="hKJ0OPf1KIUD5AI0J5C1Q1Pvoeeuru3ryyWIWDu4z38" />
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
