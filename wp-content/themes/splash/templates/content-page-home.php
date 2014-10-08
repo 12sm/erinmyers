197b387b57442509b24c4908ae09b0bda1c7e8c6
michael <michael@12southmusic.com>
2014-10-08 16:21:24 -0400

    modified theme `Theme for CLIENT NAME` version 6.5.1

--- wp-content/themes/splash/templates/content-page-home.php
+++ wp-content/themes/splash/templates/content-page-home.php
@@ -1,10 +1,10 @@
 <div class="container">
 		<div class="col-sm-6 col-sm-offset-3">
-		<img src="../wp-content/themes/12SM/assets/img/logo.png" class="img-responsive">
+		<img src="../wp-content/themes/splash/assets/img/logo.png" class="img-responsive">
 			<h3 class="headline">Website Coming Soon...</h3>
 			<ul class="socials">
 				<li class="icons"><a href="https://www.facebook.com/pages/Erin-Myers-Spiral-Spine/775348142484624" target="_blank"><i class='fa fa-facebook-square'></i></a></li>
 				<li class="icons"><a href="https://www.linkedin.com/profile/view?id=356451800" target="_blank"><i class="fa fa-linkedin-square"></i></a></li>
-				<li class="icons amazon"><a href="http://amzn.com/1475219466" target="_blank"><img src="../wp-content/themes/12SM/assets/img/amazon.png" class="img-responsive"></a></li>
+				<li class="icons amazon"><a href="http://amzn.com/1475219466" target="_blank"><img src="../wp-content/themes/splash/assets/img/amazon.png" class="img-responsive"></a></li>
 			</ul>
 </div>
