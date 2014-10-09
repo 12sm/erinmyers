205f6021ba949f2ec3e8f069561cae8e0f108790
ian12sm <ian@12southmusic.com>
2014-10-09 00:00:07 -0400

    modified theme `Theme for Erin Myers / Spiral Spine` version 6.5.1

--- wp-content/themes/spiralspine/templates/entry-meta.php
+++ wp-content/themes/spiralspine/templates/entry-meta.php
@@ -1,1 +1,4 @@
-<time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+<p class="blog-roll-meta">
+	 <?php echo get_the_category_list(', '); ?> | <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+</p>
+<div class="addthis_sharing_toolbox" data-url="<?php the_permalink(); ?>" data-title="<?php the_title(); ?>"></div>
--- wp-content/themes/spiralspine/templates/footer.php
+++ wp-content/themes/spiralspine/templates/footer.php
@@ -56,8 +56,8 @@
        wp_nav_menu(array('theme_location' => 'social_navigation', 'menu_class' => ''));
      endif;
   ?>
+        <p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></p>
   </div>
-      <p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></p>
     </div>
   </div>
 </div>  
